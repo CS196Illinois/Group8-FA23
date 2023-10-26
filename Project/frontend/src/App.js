26d7e93f2fdd028d9577c8da392a8e15d86259f3
farhanx3324 <145164130+farhanx3324@users.noreply.github.com>
2023-10-25 19:56:36 -0500

    edited text on website

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route } 
 from 'react-router-dom';
-
+import Home from './Components/Pages/HomePage/Home';
 
 function App () {
     return (
         <Router>
             <Navbar />
             <Routes>
-                <Route path='/' />
+                <Route path='/home' element={<Home/>}/>
             </Routes>
         </Router>
     );
