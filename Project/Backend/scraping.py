45bb58772258fac395eff3f17e828911eb370df5
adame <elbahey2@illinois.edu>
2023-11-06 18:58:09 -0800

    added_model

--- Project/Backend/scraping.py
+++ Project/Backend/scraping.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import yfinance as yf
+import pandas as pd
 app = Flask (__name__)
 @app.route('/')
 def index():
@@ -11,6 +12,14 @@ def stockinfo(ticker):
     hist = stock.history(period = "1d")
 
     return stock.history_metadata
+@app.route('/<ticker>prediction')
+def model(ticker):
+    stock = yf.Ticker(ticker)
+    data = pd.DataFrame()
+    df = stock.history(period = "max")
+    data[stock] = data[stock].append(df)
+    
+
 
 if __name__ == "__main__":
     app.run() 
